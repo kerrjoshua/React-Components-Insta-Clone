348a566ac692c03ab35551f62638cd2ca8087bad
Joshua Kerr <kerrjoshua@gmail.com>
2023-01-05 15:36:11 +0900

    Comments are working.

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
+import { v4 as uuid } from 'uuid';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
@@ -9,6 +10,11 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment => {
+
+        return <Comment comment={comment} key={uuid()} />;
+
+      })}
     </div>
   );
 };
